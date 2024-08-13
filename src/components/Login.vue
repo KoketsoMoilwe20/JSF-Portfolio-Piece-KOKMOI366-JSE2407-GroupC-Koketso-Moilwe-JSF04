586ba710084bff970bc0099598b29245e227a891
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-13 15:36:49 +0200

    Add basic login form structure

---
+++ src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <form>
+            <div>
+                <label>Username</label>
+                <input type="text" id="username" required />
+            </div>
+        </form>
+    </div>
+</template>
