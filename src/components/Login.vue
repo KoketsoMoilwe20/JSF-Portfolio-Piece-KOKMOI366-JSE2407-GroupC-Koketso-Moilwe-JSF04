5e270b6b21679d190ec83bcf24019bb518181b15
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-13 19:09:10 +0200

    Add password visibility toggle logic

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,3 +9,14 @@
         </form>
     </div>
 </template>
+
+<script>
+    import { ref, reactive } from 'vue';
+    import { useRouter } from 'vue-router';
+
+    const passwordVisible = ref(false);
+
+    const togglePasswordVisibility = () => {
+        passwordVisible.value = !passwordVisible;
+    };
+</script>
