0050379e6e2d24956051b3a5ec6cf894759a8160
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-14 11:38:19 +0200

    Add basic template for the Cart page

---
+++ src/components/Cart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Your Shopping Cart</h2>
+        <div>
+            Your cart is empty...
+        </div>
+        <div>
+            <img :src="product.image" alt="Product Image">
+            <div>
+                <h3>{{ product.title }}</h3>
+                <p>${{ product.price }}</p>
+                <button>Remove</button>
+            </div>
+        </div>
+        <div>
+            <p>Total Items: </p>
+            <p>Total Cost: </p>
+        </div>
+        <button>Clear Cart</button>
+    </div>
+</template>
+
+<script setup>
+    import {ref, onMounted} from 'vue';
+</script>
