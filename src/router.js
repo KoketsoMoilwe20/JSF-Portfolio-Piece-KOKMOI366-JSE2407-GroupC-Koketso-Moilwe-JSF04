e2e70b4fe6f2f039aff508b7aee97fc36f3efe31
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-14 10:00:15 +0200

    set requiresAuth meta field for Home route

--- src/router.js
+++ src/router.js
@@ -8,7 +8,8 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: ProductGrid
+        component: ProductGrid,
+        meta: {requiresAuth: true}
     },
     {
         path: '/product/:id',
