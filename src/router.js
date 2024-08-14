581e4ff219839202ca86764e0c7b22884ea04f14
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-14 11:38:02 +0200

    Create routing path to Cart page
    And add comments for the global guard registration and the conditional rendering inside of it

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import ProductGrid from './components/ProductGrid.vue'
 import ProductDetails from './components/ProductDetails.vue'
 import Login from './components/Login.vue'
+import Cart from './components/Cart.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 
@@ -21,6 +22,12 @@ const routes = [
         name: 'Login', 
         component: Login
     },
+    {
+        path: '/cart',
+        name: 'Cart',
+        component: Cart,
+        meta: {requiresAuth: true} //protecting cart route
+    },
 ]
 
 const router = createRouter({
@@ -28,8 +35,9 @@ const router = createRouter({
     routes
 });
 
+//registers a global guard that'll run before every route change
 router.beforeEach((to, from, next) => {
-    const isAuthenticated = !!localStorage.getItem('token');
+    const isAuthenticated = !!localStorage.getItem('token'); //checking authentication. Retrieves a token from localStorage (if token exists, isAuthenticated will be true, otherwise it will be false)
     
     if (to.meta.requiresAuth && !isAuthenticated) {
       next({ name: 'Login' });
@@ -39,5 +47,11 @@ router.beforeEach((to, from, next) => {
       next();
     }
   });
+
+  //Condition 1 checks if the target route (to) requires authentication
+
+  //Condition 2 checks if the user is trying to navigate to the Login page while already authenticated
+
+  //Final condition allows navigation
 
 export default router
