f03d6f2a394c1b3b4963deed7950d4c6ba6c8619
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-15 20:35:22 +0200

    Basic Comparison List Structure

--- src/components/ComparisonList.vue
+++ src/components/ComparisonList.vue
@@ -1,3 +1,17 @@
 <template>
-    
+    <div>
+        <h2>Comparison List</h2>
+
+        <!-- Displaying products side by side for comparison -->
+        <div>
+            <div clas="product-comparison">
+                <img class="product-image">
+                <h3></h3>
+                <p>Price: </p>
+                <p>Category: </p>
+                <button class="remove-button">Remove</button>
+            </div>
+        </div>
+
+    </div>
 </template>
