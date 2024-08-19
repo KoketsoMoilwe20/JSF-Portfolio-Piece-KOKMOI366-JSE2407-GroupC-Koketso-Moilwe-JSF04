f490b7f5e2805751fa7af32f2314a04e98ad1906
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-19 09:15:07 +0200

    Protect comparison list page

--- src/router.js
+++ src/router.js
@@ -32,6 +32,7 @@ const routes = [
       path: '/comparison',
       name: 'ComparisonList',
       component: ComparisonList,
+      meta: {requiresAuth: true} //protect comparison route
     }
 ]
 
